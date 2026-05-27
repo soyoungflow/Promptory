9ef12fe60f6e68a3f9e00b2b34a575d5d76bfe3d
Soyoung Park <sypark03131@gmail.com>
2026-05-27 21:32:33 +0900

    d

--- config/settings/production.py
+++ config/settings/production.py
@@ -24,9 +24,9 @@ ALLOWED_HOSTS = build_allowed_hosts(config('ALLOWED_HOSTS', default=''))
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     config('DB_NAME'),
-        'USER':     config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME':     config('DB_NAME', default='promptory_db'),
+        'USER':     config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
         'HOST':     config('DB_HOST', default='localhost'),
         'PORT':     config('DB_PORT', default='5432'),
         'OPTIONS': {
